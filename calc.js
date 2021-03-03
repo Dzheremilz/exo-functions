cefd4c9356212513024b0d9e9f51cbde321ca9ef
Dzheremilz <trempejeremie@gmail.com>
2021-03-03 15:10:04 +0100

    fix

--- calc.js
+++ calc.js
@@ -14,16 +14,16 @@ const div = (nb1, nb2) => {
   return nb1 / nb2
 }
 
-const calc = (Op, nb1, nb2) => {
-  if (Op === '+') {
+const calc = (operator, nb1, nb2) => {
+  if (operator === '+') {
     return add(nb1, nb2)
-  } else if (Op === '-') {
+  } else if (operator === '-') {
     return sub(nb1, nb2)
-  } else if (Op === '*') {
+  } else if (operator === '*') {
     return mul(nb1, nb2)
-  } else if (Op === '/') {
+  } else if (operator === '/') {
     return div(nb1, nb2)
   }
 }
 
-console.log(calc('/', 2, 3))
+console.log(calc('*', 10, 2))
