3d835d4572d9e26ea838a2aaa91fc834b3ecd7f7
Dzheremilz <trempejeremie@gmail.com>
2021-03-03 17:51:07 +0100

    ternary

--- parity.js
+++ parity.js
@@ -1,13 +1,9 @@
 const isOdd = (nb) => {
-  if (nb % 2 === 1) {
-    return true
-  } else {
-    return false
-  }
+  return nb % 2 === 1 ? true : false
 }
 
 const isEven = (nb) => {
   return !isOdd(nb)
 }
 
-console.log(isEven(77))
+console.log(isEven(78))
