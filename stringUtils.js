c4308cab88666d80893c0e2115f1e0ebea624ee9
Dzheremilz <trempejeremie@gmail.com>
2021-03-04 00:39:44 +0100

    gn

--- stringUtils.js
+++ stringUtils.js
@@ -2,6 +2,20 @@ const reverseStr = (str) => {
   return str.split('').reverse().join('')
 }
 
+const reverseStr2 = (str) => {
+  return (str === '') ? '' : reverseStr2(str.substr(1)) + str.charAt(0);
+}
+
+const reverseStr3 = (str) => {
+  let str2 = str.split('')
+  for (let i = 0, j = str2.length - 1; i < (str2.length / 2); i++) {
+    [str2[i], str2[j]] = [str2[j], str2[i]]
+    j--;
+  }
+  str2 = str2.join('')
+  return str2
+}
+
 const isPalindrome = (str) => {
   str = str.toUpperCase()
   let temp = reverseStr(str)
@@ -9,4 +23,7 @@ const isPalindrome = (str) => {
 }
 
 console.log(reverseStr('Hello!'))
+console.log(reverseStr2('Hello!'))
+console.log(reverseStr3('AbCdEfGhIjKlMnOpQrStUvWxYz!'))
+
 console.log(isPalindrome('Kayak laval kaYak'))
