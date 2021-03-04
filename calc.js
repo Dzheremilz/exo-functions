d066d5df95984e63f9f0345e8561a14f59c9e11e
Dzheremilz <trempejeremie@gmail.com>
2021-03-04 11:34:13 +0100

    case

--- calc.js
+++ calc.js
@@ -14,6 +14,21 @@ const div = (nb1, nb2) => {
   return nb1 / nb2
 }
 
+const power = (nb1, nb2) => {
+  return nb1 ** nb2
+}
+
+const calc = (operator, nb1, nb2) => {
+  switch (operator) {
+    case '+': return add(nb1, nb2)
+    case '-': return sub(nb1, nb2)
+    case '*': return mul(nb1, nb2)
+    case '/': return div(nb1, nb2)
+    case '**': return power(nb1, nb2)
+  }
+}
+
+/*
 const calc = (operator, nb1, nb2) => {
   if (operator === '+') {
     return add(nb1, nb2)
@@ -25,5 +40,6 @@ const calc = (operator, nb1, nb2) => {
     return div(nb1, nb2)
   }
 }
+*/
 
-console.log(calc('*', 10, 2))
+console.log(calc('**', 10, 2))
